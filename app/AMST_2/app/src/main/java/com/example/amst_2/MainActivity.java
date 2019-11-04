00a6a6248b57cbc5d33cf96017bd3691f37eb3c3
bolisteward <bolisteward@users.noreply.github.com>
2019-11-03 19:57:52 -0600

    boton flotante y mapa

---
+++ app/AMST_2/app/src/main/java/com/example/amst_2/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.amst_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG ="MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void playVideo(View view){
+        Intent intent = new Intent(this,videoView.class);
+        startActivity(intent);
+    }
+
+
+    public void graficoLineal(View view){
+        Intent intent = new Intent(this,LinealGraph.class);
+        startActivity(intent);
+    }
+
+    public void verMap(View view){
+        if(isServiceOk()){
+            Intent intent = new Intent(this,myMap.class);
+            startActivity(intent);
+        }
+    }
+    public void verCalendario(View view){
+        Intent intent = new Intent(this,calendar.class);
+        startActivity(intent);
+    }
+
+
+    public boolean isServiceOk(){
+        Log.d(TAG, "isServiceOk: checking google service version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if (available == ConnectionResult.SUCCESS){
+            //Everything is fine and the user can make map request
+            return true;
+        } else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error ocured but we can resolt it
+            Log.d(TAG, "isServiceOk: an error occures but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map request", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+}
